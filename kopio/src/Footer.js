bf6308e077c0db85f60d360d136bf5d47b481c94
Heudiini <77001762+Heudiini@users.noreply.github.com>
2022-04-16 12:36:40 +0100

    good

---
+++ kopio/src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./css/App.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>2022 code by Heudiini</small>
+    </footer>
+  );
+}
---
+++ kopio/src/Forecast.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./css/Forecast.css";
+
+export default function SearchEngine() {
+  const [city, setCity] = useState("");
+  const [result, setResult] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function showTemp(response) {
+    setResult(true);
+    console.log(response.data);
+    setWeather({
+      city: response.data.name,
+      country: response.data.sys.country,
+      weather: response.data,
+      visibility: response.data.main.visibility,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      sunrise: response.data.sunrise,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = `cf35cd803ef0202f5f034abcff722764`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(showTemp);
+    console.log(url);
+  }
+
+  function changeCity(event) {
+    setCity(event.target.value);
+    //console.log(event.target.value)
+  }
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Type a city" onChange={changeCity} />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (result) {
+    return (
+      <div className="container">
+        {form}
+        <h3>
+          {weather.city}, {weather.country}{" "}
+        </h3>
+        <ul>
+          <li>
+            <h4>Temperature: {Math.round(weather.temperature)}°C</h4>
+          </li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
